f8bd27272ff3e90187e6df7f217aab96b66a35e9
codymartins <codymarti778@gmail.com>
2025-06-25 21:27:12 -0400

    Add files via upload

---
+++ resource_library.dart
@@ -0,0 +1,76 @@
+const resourceLibrary = {
+  1: [ // Phase 1
+    {
+      'title': 'Ego is the Enemy (Ryan Holiday)',
+      'type': 'Book',
+      'url': 'https://www.amazon.com/Ego-Enemy-Ryan-Holiday/dp/1591847818',
+    },
+    {
+      'title': 'Jordan Peterson on Meaning',
+      'type': 'Video',
+      'url': 'https://www.youtube.com/watch?v=Z4pH4gV7xU4',
+    },
+    {
+      'title': 'Wake Up: Stop Numbing Your Mind',
+      'type': 'Article',
+      'url': 'https://someblog.com/wake-up-article',
+    },
+    {
+      'title': 'Daily Stoic “Start with who you are” Episodes',
+      'type': 'Podcast',
+      'url' : 'https://dailystoic.com/podcast/',
+    },
+    {
+      'title': 'Responsibility, identity, chaos vs. order',
+      'type': 'Video',
+      'url' : 'https://youtu.be/O6-zTxWXnCo?si=_sz61Yp46QEhgrda',
+    },
+    {
+      'title': 'Discover Yourself ',
+      'type': 'Book',
+      'url' : 'https://scottschwefel.com/wp-content/uploads/2020/05/Discover-Yourself-by-Scott-Schwefel-Copyright-2018.pdf',
+    },
+    {
+      'title': 'From Purpose to Impact',
+      'type': 'Article',
+      'url' : 'https://khkll.com/wp-content/uploads/2023/06/From-Purpose-to-Impact.pdf',
+    },
+  ],
+  2: [//Phase 2
+    {
+      'title': 'Modern Wisdom',
+      'type': 'Podcast',
+      'url': 'https://chriswillx.com/podcast/',
+    },
+    {
+      'title': 'Discipline = Freedom',
+      'type': 'Video',
+      'url': 'https://youtu.be/eBmVv2P-v2s?si=VyhqmcQKOaUr4ZS1',
+    },
+    {
+      'title': 'Order of Man ',
+      'type': 'Podcast',
+      'url': 'https://www.orderofman.com/podcast/',
+    },
+    {
+      'title': 'Do the Work',
+      'type': 'Book',
+      'url' : 'https://www.amazon.com/Do-Work-Overcome-Resistance-Your/dp/1936891379/ref=sr_1_1?crid=1XU3EKWGR356U&dib=eyJ2IjoiMSJ9.QkqyYZIKLi-9R5DWQoh6-YWeCjTC0DI7UfL35zgpXayQLXQrR78OiFbpgLjkjimZ2OjEdKr3xPa1w20iy3X3t0sw8CsGu5kpwg6E0kfp6aNVYhGYErFMKO_AQgfbJgZeq2PFGf-z5IzGZcFD-WThKC_YH-8LsZxYuYxNfDXyRxr9b0cCCvAc9fqEwIsoWjzlCMeIn8QwvD7Tn4muaU8hCjgZT9S5-JvIozzno_HVKuYvN26XflrTV1sAs4b887tb__QwAQESotMOd6hDktraPePLGoG4HDN98NFrO4FGi-M.nfnFesv075cOPG-VTadLb3k7ttKTZD97TCh7i3nCuNQ&dib_tag=se&keywords=do+the+work&qid=1744900663&s=books&sprefix=do+the+work,stripbooks,130&sr=1-1',
+    },
+    {
+      'title': 'The Comfort Crisis',
+      'type': 'Book',
+      'url' : 'https://www.amazon.com/Comfort-Crisis-Embrace-Discomfort-Resilience/dp/0593139136',
+    },
+    {
+      'title': 'Atomic Habits ',
+      'type': 'Book',
+      'url' : 'https://www.amazon.com/Atomic-Habits-James-Clear/dp/0735211299',
+    },
+    {
+      'title': 'Make Your Bed',
+      'type': 'Speech',
+      'url' : 'https://www.youtube.com/watch?v=sBAqF00gBGk',
+    },
+  ]
+};
